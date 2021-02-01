a4b666ca47189230cb7976dba7ee521535842661
Ariel_Ibarra <arielibarra39@gmail.com>
2021-02-01 14:38:10 -0800

    add models & migrate db

---
+++ db/migrate/20210201223529_create_nonprofits.rb
@@ -0,0 +1,14 @@
+class CreateNonprofits < ActiveRecord::Migration[6.0]
+  def change
+    create_table :nonprofits do |t|
+      t.string :name
+      t.string :location
+      t.string :description
+      t.string :contact_info
+      t.string :link
+      t.integer :category_id
+
+      t.timestamps
+    end
+  end
+end
